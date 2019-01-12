4769514797e9ebb068d72d96490ffa4cc6327da0
bel7aG <belhassen.gharsallah@isetrades.com>
2019-01-12 23:59:43 +0100

    ...

---
+++ src/playground/router-one/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <header>
+    wassup this is header 
+  </header>
+)
+
+export default Header
---
+++ src/playground/router-one/router/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter as Router, Router, withRouter, Switch } from 'react-router-dom'
+
+const AuthExample = () => (
+  <Router>
+    <>
+    </>
+  </Router>
+)
+
+export default Router
