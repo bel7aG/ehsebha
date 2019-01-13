a7aafbb53a0ce9c3a5d42ec9411e2c03cb5329da
bel7aG <belhassen.gharsallah@isetrades.com>
2019-01-13 12:36:55 +0100

    Updates

---
+++ precache-manifest.ab2ac21522b213c16c25749b9383d934.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "48613c64d1dfb8bdd6e0",
+    "url": "/ehsebha/static/js/runtime~main.48613c64.js"
+  },
+  {
+    "revision": "60ab0d17e305f1888fa9",
+    "url": "/ehsebha/static/js/main.60ab0d17.chunk.js"
+  },
+  {
+    "revision": "ac5061da54c65d4799c1",
+    "url": "/ehsebha/static/js/1.ac5061da.chunk.js"
+  },
+  {
+    "revision": "86efa5841275402f59678ba9d22e070e",
+    "url": "/ehsebha/index.html"
+  }
+];
